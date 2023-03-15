c841d15bef772a8f23f9087ef6f76f3e4585668f
Niels Santiago Baltodano Valencia <58533885+niels-baltodano@users.noreply.github.com>
2023-03-15 18:50:33 -0500

    Update getPodTemplatePR.groovy

--- vars/getPodTemplatePR.groovy
+++ vars/getPodTemplatePR.groovy
@@ -3,82 +3,14 @@ def call() {
 apiVersion: v1
 kind: Pod
 metadata:
-    name: jenkins-slave
+  namespace: default
+  labels:
+    job: bootvar-build-pod
 spec:
-  serviceAccountName: myjenkins
   containers:
-    - name: jnlp
-      image: jenkins/inbound-agent:4.11.2-4
-      imagePullPolicy: Always
-      args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
-      env:
-        - name: GIT_SSL_NO_VERIFY
-          value: "true"
-      resources:
-        requests:
-            cpu: 10m
-            memory: 256Mi
-        limits:
-            cpu: 512m
-            memory: 1Gi
-    - name: maven
-      image: maven:3.8.5-openjdk-11-slim
-      imagePullPolicy: Always
-      command: ['cat']
-      tty: true
-      resources:
-        requests:
-          cpu: 512m
-          memory: 512Mi
-        limits:
-          memory: 1Gi
-          cpu: 1024m
-          ephemeral-storage: 1Gi
-      securityContext:
-        runAsUser: 0
-    - name: node
-      image: node:lts-stretch-slim
-      imagePullPolicy: Always   
-      command: ['cat']
-      tty: true
-      resources:
-        requests:
-          cpu: 100m
-          memory: 256Mi
-        limits:
-          memory: 1Gi
-          cpu: 1024m
-          ephemeral-storage: 1Gi
-      securityContext:
-        runAsUser: 0
-    - name: kaniko
-      image: gcr.io/kaniko-project/executor:debug
-      imagePullPolicy: Always   
-      command: ['cat']
-      tty: true
-      resources:
-        requests:
-          cpu: 200m
-          memory: 512Mi
-        limits:
-          memory: 1Gi
-          cpu: 1024m
-          ephemeral-storage: 1Gi
-      securityContext:
-        runAsUser: 0
-      env:
-       - name: GOOGLE_APPLICATION_CREDENTIALS
-         value: /kaniko/.docker
-      volumeMounts:
-        - name: kaniko-secret
-          mountPath: /kaniko/.docker
-          readOnly: false
-  volumes:
-  - name: kaniko-secret                                 
-    secret:
-      secretName: pull-secret-docker-hub
-      items:
-      - key: .dockerconfigjson
-        path: config.json
+  - name: bootvar-container
+    image: alpine:latest
+    tty: true
+    command: ['cat']
 """
 }
