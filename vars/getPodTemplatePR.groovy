6662270ea4dd5b58366b80ab4b7c5eed5401aff7
Niels Santiago Baltodano Valencia <58533885+niels-baltodano@users.noreply.github.com>
2023-03-15 19:38:40 -0500

    Update getPodTemplatePR.groovy

--- vars/getPodTemplatePR.groovy
+++ vars/getPodTemplatePR.groovy
@@ -3,78 +3,76 @@ def call() {
 apiVersion: v1
 kind: Pod
 metadata:
-  namespace: default
   labels:
     app: jenkins-agent
 spec:
-  serviceAccountName: jenkins-local
   containers:
-    - name: jnlp
-      image: jenkins/inbound-agent:4.11.2-4
-      imagePullPolicy: Always
-      args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
-      env:
-        - name: GIT_SSL_NO_VERIFY
-          value: "true"
-      resources:
-        requests:
-            cpu: 10m
-            memory: 256Mi
-        limits:
-            cpu: 512m
-            memory: 1Gi
-    - name: maven
-      image: maven:3.8.5-openjdk-11-slim
-      imagePullPolicy: Always
-      command: ['cat']
-      tty: true
-      resources:
-        requests:
-          cpu: 512m
-          memory: 512Mi
-        limits:
-          memory: 1Gi
-          cpu: 1024m
-          ephemeral-storage: 1Gi
-      securityContext:
-        runAsUser: 0
-    - name: node
-      image: node:lts-stretch-slim
-      imagePullPolicy: Always   
-      command: ['cat']
-      tty: true
-      resources:
-        requests:
-          cpu: 100m
+  - name: jnlp
+    image: jenkins/inbound-agent:4.11.2-4
+    imagePullPolicy: Always
+    args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
+    env:
+      - name: GIT_SSL_NO_VERIFY
+        value: "true"
+    resources:
+      requests:
+          cpu: 10m
           memory: 256Mi
-        limits:
+      limits:
+          cpu: 512m
           memory: 1Gi
-          cpu: 1024m
-          ephemeral-storage: 1Gi
-      securityContext:
+  - name: maven
+    image: maven:3.8.5-openjdk-11-slim
+    imagePullPolicy: Always
+    command: ['cat']
+    tty: true
+    resources:
+      requests:
+        cpu: 512m
+        memory: 512Mi
+      limits:
+        memory: 1Gi
+        cpu: 1024m
+        ephemeral-storage: 1Gi
+    securityContext:
         runAsUser: 0
-    - name: kaniko
-      image: gcr.io/kaniko-project/executor:debug
-      imagePullPolicy: Always   
-      command: ['cat']
-      tty: true
-      resources:
-        requests:
-          cpu: 200m
-          memory: 512Mi
-        limits:
-          memory: 1Gi
-          cpu: 1024m
-          ephemeral-storage: 1Gi
-      securityContext:
-        runAsUser: 0
-      env:
-       - name: GOOGLE_APPLICATION_CREDENTIALS
-         value: /kaniko/.docker
-      volumeMounts:
-        - name: kaniko-secret
-          mountPath: /kaniko/.docker
-          readOnly: false
+  - name: node
+    image: node:lts-stretch-slim
+    imagePullPolicy: Always   
+    command: ['cat']
+    tty: true
+    resources:
+      requests:
+        cpu: 100m
+        memory: 256Mi
+      limits:
+        memory: 1Gi
+        cpu: 1024m
+        ephemeral-storage: 1Gi
+    securityContext:
+      runAsUser: 0
+  - name: kaniko
+    image: gcr.io/kaniko-project/executor:debug
+    imagePullPolicy: Always   
+    command: ['cat']
+    tty: true
+    resources:
+      requests:
+        cpu: 200m
+        memory: 512Mi
+      limits:
+        memory: 1Gi
+        cpu: 1024m
+        ephemeral-storage: 1Gi
+    securityContext:
+      runAsUser: 0
+    env:
+      - name: GOOGLE_APPLICATION_CREDENTIALS
+        value: /kaniko/.docker
+    volumeMounts:
+      - name: kaniko-secret
+        mountPath: /kaniko/.docker
+        readOnly: false
   volumes:
   - name: kaniko-secret                                 
     secret:
