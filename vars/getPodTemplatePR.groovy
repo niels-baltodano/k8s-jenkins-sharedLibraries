05e8667bf3f797a5c1e21671871554e9ccc56c77
Niels Santiago Baltodano Valencia <58533885+niels-baltodano@users.noreply.github.com>
2023-03-15 21:59:02 -0500

    Update getPodTemplatePR.groovy

--- vars/getPodTemplatePR.groovy
+++ vars/getPodTemplatePR.groovy
@@ -2,29 +2,81 @@ def call() {
 """
 metadata:
   labels:
-    some-label: some-label-value
+    app: jenkins-agent
 spec:
   containers:
   - name: jnlp
+    image: 'jenkins/inbound-agent'
+    imagePullPolicy: Always
+    args: ['\$(JENKINS_SECRET)', '\$(JENKINS_NAME)']
     env:
-    - name: CONTAINER_ENV_VAR
-      value: jnlp
+      - name: GIT_SSL_NO_VERIFY
+        value: "true"
+    resources:
+      requests:
+          cpu: 10m
+          memory: 256Mi
+      limits:
+          cpu: 512m
+          memory: 1Gi
   - name: maven
-    image: maven:3.8.1-jdk-8
-    command:
-    - sleep
-    args:
-    - 99d
-    env:
-    - name: CONTAINER_ENV_VAR
-      value: maven
-  - name: busybox
-    image: busybox
-    command:
-    - cat
+    image: 'maven:3.8.5-openjdk-11-slim'
+    imagePullPolicy: Always
+    command: ['cat']
     tty: true
+    resources:
+      requests:
+        cpu: 512m
+        memory: 512Mi
+      limits:
+        memory: 1Gi
+        cpu: 1024m
+        ephemeral-storage: 1Gi
+    securityContext:
+        runAsUser: 0
+  - name: node
+    image: 'node:lts-stretch-slim'
+    imagePullPolicy: Always   
+    command: ['cat']
+    tty: true
+    resources:
+      requests:
+        cpu: 100m
+        memory: 256Mi
+      limits:
+        memory: 1Gi
+        cpu: 1024m
+        ephemeral-storage: 1Gi
+    securityContext:
+      runAsUser: 0
+  - name: kaniko
+    image: gcr.io/kaniko-project/executor:debug
+    imagePullPolicy: Always   
+    command: ['cat']
+    tty: true
+    resources:
+      requests:
+        cpu: 200m
+        memory: 512Mi
+      limits:
+        memory: 1Gi
+        cpu: 1024m
+        ephemeral-storage: 1Gi
+    securityContext:
+      runAsUser: 0
     env:
-    - name: CONTAINER_ENV_VAR
-      value: busybox
+      - name: GOOGLE_APPLICATION_CREDENTIALS
+        value: /kaniko/.docker
+    volumeMounts:
+      - name: kaniko-secret
+        mountPath: /kaniko/.docker
+        readOnly: false
+  volumes:
+  - name: kaniko-secret                                 
+    secret:
+      secretName: pull-secret-docker-hub
+      items:
+      - key: .dockerconfigjson
+        path: config.json
 """
 }
